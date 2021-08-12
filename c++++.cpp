980c691c7c8ed3f984a21b2e8fe6663207334061
bantita64010454 <88584411+bantita64010454@users.noreply.github.com>
2021-08-12 18:10:49 +0700

    Add files via upload

---
+++ c++++.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	int c,a,b;
+	 scanf("%d %d %d", &c,&a,&b);
+	 
+	 c = (2*a)+b;
+	 printf("1st equation %d\n",c);
+	 
+	 c= (a*a*a*a*a)+(a*a*a*a)+ (2*(a*a*a)) + (3*(a*a*a))+3;
+	 printf("second equation %d\n",c);
+	 
+	 c = 2+(3*a);
+	  printf("third equation %d\n",c);
+	 
+	 
+}
