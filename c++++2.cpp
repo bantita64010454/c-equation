dc2684f8a52db675937f6d4ebe7e2cae7460bd9c
bantita64010454 <88584411+bantita64010454@users.noreply.github.com>
2021-08-12 18:17:37 +0700

    Add files via upload

---
+++ c++++2.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<math.h>
+int main() {
+	int c,a,b,a5,a4,a3;
+	 scanf("%d %d %d", &c,&a,&b);
+	 
+	 
+	 c = (2*a)+b;
+	 printf("1st equation %d\n",c);
+	 
+	 a5=pow(a,5);
+	  a4=pow(a,4);
+	   a3=pow(a,3);
+	 
+	 
+	 c= a5 + a4 + (2*a3) + (3*a3)+3;
+	 printf("second equation %d\n",c);
+	 
+	 c = 2+(3*a);
+	  printf("third equation %d\n",c);
+	 
+	 
+}
